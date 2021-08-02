9e95d5891712267aeb8f09286c5bd85a3c0c3011
Oleksandr Lavrusenko <oleksandr.lavrusenko@wikrgroup.com>
2021-08-02 18:04:49 +0300

    add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import axios from "axios";
+
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
